5b7b092bbafcb81b38390e3f5f8ad83f25696529
ImPonicornio <69733566+marceyuli@users.noreply.github.com>
2022-05-12 09:46:34 -0400

    innitial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+var numbreOfButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numbreOfButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttonInnerHtml = this.innerText;
+    playSound(buttonInnerHtml);
+    buttonAnimation(buttonInnerHtml);
+  });
+
+}
+document.addEventListener("keydown", function(event){
+  playSound(event.key);
+  buttonAnimation(event.key);
+})
+
+function playSound(letra){
+  switch (letra) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    default:
+  }
+}
+
+function buttonAnimation(key){
+  var activeButton= document.querySelector("."+key);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100);
+}
